b6f818fb082700bee61120fed46c39b2c1c2d240
Victor Li <victorli5674@yahoo.com>
2017-07-26 21:51:49 -0700

    a little refactoring and added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator
+
+Users can add, subtract, multiply and divide two numbers.
+Users can clear the input field with a clear button.
+Users can keep chaining mathematical operations together until the equal button is pressed, and the calculator will tell me the correct output.
+
+Made with HTML, CSS, and vanilla Javascript. Testing done with [**Jasmine**](https://jasmine.github.io/)
--- view.js
+++ view.js
@@ -1,40 +1,41 @@
 'use strict';
 
 var buttonNumber = document.querySelectorAll('.button__NUMBER');
+var buttonToggle = document.querySelector('.button__TOGGLE');
+var buttonCE = document.querySelector('.button__CE');
+var buttonC = document.querySelector('.button__C');
+var buttonBack = document.querySelector('.button__BACK');
+var buttonOperation = document.querySelectorAll('.button__OPERATION');
+var buttonCalc = document.querySelector('.button__CALC');
+
 buttonNumber.forEach(function (button) {
     button.addEventListener('click', function () {
         controlNumberButton(button.value);
     });
 });
 
-var buttonToggle = document.querySelector('.button__TOGGLE');
 buttonToggle.addEventListener('click', function () {
     controlToggleButton();
 });
 
-var buttonCE = document.querySelector('.button__CE');
 buttonCE.addEventListener('click', function () {
     controlCEButton();
 });
 
-var buttonC = document.querySelector('.button__C');
 buttonC.addEventListener('click', function () {
     controlCButton();
 });
 
-var buttonBack = document.querySelector('.button__BACK');
 buttonBack.addEventListener('click', function () {
     controlBackButton();
 });
 
-var buttonOperation = document.querySelectorAll('.button__OPERATION');
 buttonOperation.forEach(function (button) {
     button.addEventListener('click', function () {
         controlButtonOperation(button.value);
     });
 });
 
-var buttonCalc = document.querySelector('.button__CALC');
 buttonCalc.addEventListener('click', function () {
     controlCalcButton();
 });
